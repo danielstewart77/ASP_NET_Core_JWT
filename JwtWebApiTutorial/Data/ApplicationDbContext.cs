63a0714d405a252dc490bff59b5680743a985759
Daniel Stewart <daniel.stewart77@gmail.com>
2022-06-05 16:53:11 -0500

    Add project files.

---
+++ JwtWebApiTutorial/Data/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using JwtWebApiTutorial.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace JwtWebApiTutorial.Data
+{
+	public class Db : DbContext
+	{
+		public DbSet<User> Users { get; set; }
+
+
+		protected override void OnConfiguring(DbContextOptionsBuilder options)
+		{
+			var config = new ConfigurationBuilder()
+			.AddJsonFile("appsettings.json")
+			.Build();
+
+			options.UseSqlServer(config.GetConnectionString("DefaultConnection"));
+		}
+	}
+}
